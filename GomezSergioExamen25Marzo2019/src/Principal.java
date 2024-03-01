9a3be89f6e4fbf57facdf1b37c778262c94a9a97
SergioGomez04 <s.gomez.rosa@iescristobaldemonroy.es>
2024-03-01 17:29:03 +0100

    Update Principal.java

--- GomezSergioExamen25Marzo2019/src/Principal.java
+++ GomezSergioExamen25Marzo2019/src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
         Empleado[] misEmpleados = new Empleado[5];
         EmpleadoJefe jefe1 = new EmpleadoJefe(null, null, 0, null, null);
         EmpleadoBase base1 = new EmpleadoBase("administrativo", null, null, 1100);
-        
+        System.out.println("hola");
         //TO DO crear array para añadir los empleados
         misEmpleados[1]= jefe1;
     }
